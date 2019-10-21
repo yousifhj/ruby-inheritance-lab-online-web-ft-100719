da2dbe5b0f58fa5209916cc6931e25a95f228d53
Huda Yousif <hjdoski@gmail.com>
2019-10-21 21:42:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
 class Student < User 
 
+  def iniitialize(first_name, last_name, knowledge)
+    @first_name = first_name
+    @last_name = last_name
+    knowledge = []
+  end
+  
+  def new 
+
+  end 
 end
