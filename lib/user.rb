0d15a17b95092bdbc06466d63d528cab0df0e147
Huda Yousif <hjdoski@gmail.com>
2019-10-21 21:41:20 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,14 +2,14 @@ class User
   
   attr_accessor :first_name, :last_name
   
-  def iniitialize(first_name, last_name)
+  def iniitialize(first_name, last_name, knowledge)
     @first_name = first_name
     @last_name = last_name
     knowledge = []
   end
   
   def new 
-    knowledge = []
+
   end 
   
 end 
